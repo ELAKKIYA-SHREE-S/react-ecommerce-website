4dd4bcd2a1e5e3fb59dcbe4480f0283c5a3457cd
ELAKKIYA SHREE S <elakkiyashree.s2021csea@sece.ac.in>
2024-04-01 18:56:45 +0530

    initial commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +91 9361554848 </label>
+            <i className='fa fa-envelope'></i>
+            <label> elakkiyashree.s2021csea@sece.ac.in </label>
+          </div>
+          <div className='right row RText'>
+            <label>FAQ's</label>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>IND</label>
+            <span>🏳️‍⚧️</span>
+            <label>INR</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
